69114f21239d02948d7eef6fe7c17b730f27214b
rashidg <rashid.gaziyev@gmail.com>
2017-06-14 18:38:50 -0400

    Add DRF and /stores/ endpoint

--- pholo/urls.py
+++ pholo/urls.py
@@ -1,20 +1,41 @@
-"""pholo URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+
+#Models
+from django.db import models
+
+class Store(models.Model):
+    name = models.CharField(max_length=30)
+    desc = models.CharField(max_length=30)
+
+
+class StoreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Store
+        fields = ('name', 'desc')
+
+class StoreViewSet(viewsets.ModelViewSet):
+	queryset = Store.objects.all()
+	serializer_class = StoreSerializer
+
+
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'stores', StoreViewSet)
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(router.urls))
 ]
+
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+# urlpatterns = [
+#     url(r'^', include(router.urls)),
+#     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+# ]
