3926e6b1de623e8eff2e7b31954e44c5e3e4680c
rashidg <rashid.gaziyev@gmail.com>
2017-06-17 17:03:25 -0400

    Add Table Model

--- pholo/urls.py
+++ pholo/urls.py
@@ -11,21 +11,45 @@ class Store(models.Model):
     name = models.CharField(max_length=30)
     desc = models.CharField(max_length=30)
 
+    def __str__(self):
+		return "Store: " + self.name
+
+class Table(models.Model):
+	number = models.CharField(max_length=30)
+	status = models.CharField(max_length=30)
+	store = models.ForeignKey(Store, default=1)
+
+	def __str__(self):
+		return 'Table: ' + self.number
+
+admin.site.register(Store)
+admin.site.register(Table)
 
 class StoreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Store
         fields = ('name', 'desc')
 
+class TableSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Table
+		fields = ('number', 'status')
+
 class StoreViewSet(viewsets.ModelViewSet):
 	queryset = Store.objects.all()
 	serializer_class = StoreSerializer
 
+class TableViewSet(viewsets.ModelViewSet):
+	serializer_class = TableSerializer
 
+	def get_queryset(self):
+		store_id = self.kwargs.get('store_id')
+		return Table.objects.filter(store=store_id)
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'stores', StoreViewSet)
+router.register(r'stores/(?P<store_id>[0-9]+)/tables', TableViewSet, base_name='tables')
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
